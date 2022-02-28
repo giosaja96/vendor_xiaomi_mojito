34e42d396581e3da80c437d3c9eb330353725332
boedhack99 <boedhack@gmail.com>
2022-02-28 13:34:33 +0700

    mojito: Import separate keymaster@4.1 manifest from stock

--- mojito-vendor.mk
+++ mojito-vendor.mk
@@ -1271,6 +1271,7 @@ PRODUCT_PACKAGES += \
     qcrilmsgtunnel \
     WfdCommon \
     android.hardware.gnss@2.1-service-qti \
+    android.hardware.keymaster@4.1-service-default-qti \
     android.hardware.neuralnetworks@1.3-service-qti-hta \
     android.hardware.neuralnetworks@1.3-service-qti \
     manifest_android.hardware.drm@1.3-service.widevine \
